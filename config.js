5bb2655f683edce8f1ad7f94421af51b2661e2e5
Winstonshih <xxinfernobladexx@gmail.com>
2025-01-15 16:22:07 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    OpenAIAPIKey: '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'
+};
